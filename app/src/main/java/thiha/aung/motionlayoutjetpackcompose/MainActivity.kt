105862245b3cefe23cd028f4bf44d2cc17e4a4f5
Aung Thiha <mr.aungthiha@gmail.com>
2023-08-27 17:36:27 +0700

    feat: moving image in MotionLayout with a slider

---
+++ app/src/main/java/thiha/aung/motionlayoutjetpackcompose/MainActivity.kt
@@ -0,0 +1,117 @@
+package thiha.aung.motionlayoutjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+import androidx.constraintlayout.compose.layoutId
+import thiha.aung.motionlayoutjetpackcompose.ui.theme.MotionLayoutJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MotionLayoutJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        val progress = remember {
+                            mutableStateOf(0f)
+                        }
+                        MotionLayoutSample(progress = progress.value)
+                        Slider(
+                            value = progress.value,
+                            onValueChange = {
+                                progress.value = it
+                            },
+                            modifier = Modifier
+                                .padding(horizontal = 32.dp)
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun MotionLayoutSample(progress: Float) {
+    val context = LocalContext.current
+    val motionSceneContent = remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+    MotionLayout(
+        motionScene = MotionScene(motionSceneContent),
+        progress = progress,
+        modifier = Modifier
+            .fillMaxWidth(),
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_launcher_foreground
+            ),
+            contentDescription = stringResource(R.string.app_icon),
+            modifier = Modifier
+                .layoutId("my_image")
+        )
+        Divider(
+            color = Color.Gray,
+            thickness = 2.dp,
+            modifier = Modifier
+                .layoutId("my_divider")
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MotionLayoutJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
